3407f467e713caf1c3ce9f6b7615134b059a9e20
patrik.bergstrom <patrik.bergstrom@knowit.se>
2013-12-11 16:30:11 +0000

    Added fetching of unread emails (without body).

--- core-bc/composites/svc/src/main/java/se/vgregion/exchange/service/EwsService.java
+++ core-bc/composites/svc/src/main/java/se/vgregion/exchange/service/EwsService.java
@@ -2,37 +2,39 @@ package se.vgregion.exchange.service;
 
 import com.microsoft.schemas.exchange.services._2006.messages.*;
 import com.microsoft.schemas.exchange.services._2006.types.*;
-import com.microsoft.schemas.exchange.services._2006.types.Value;
+import com.microsoft.schemas.exchange.services._2006.types.ObjectFactory;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.frontend.ClientProxy;
-import org.apache.cxf.interceptor.LoggingInInterceptor;
-import org.apache.cxf.interceptor.LoggingOutInterceptor;
 import org.apache.cxf.transport.http.HTTPConduit;
-import org.apache.cxf.transport.https.CertificateHostnameVerifier;
 import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
-import org.springframework.beans.factory.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ldap.support.LdapUtils;
 import se.vgregion.ldapservice.LdapService;
 import se.vgregion.ldapservice.LdapUser;
 
 import javax.annotation.PostConstruct;
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Holder;
-import java.io.IOException;
-import java.net.*;
-import java.util.*;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
 
 /**
  * @author Patrik Bergström
  */
 public class EwsService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(EwsService.class);
+
+    private final ObjectFactory objectFactory = new ObjectFactory();
     private ExchangeServicePortType exchangeServicePort;
     private final LdapService ldapService;
     private final URL wsdlLocation;
@@ -77,6 +79,86 @@ public class EwsService {
         conduit.setClient(client);
     }
 
+    public List<MessageType> fetchUnreadEmails(String userId, int maxNumber) {
+
+        ConstantValueType constantValueType = objectFactory.createConstantValueType();
+        constantValueType.setValue("0");
+
+        PathToUnindexedFieldType fieldType = new PathToUnindexedFieldType();
+        fieldType.setFieldURI(UnindexedFieldURIType.MESSAGE_IS_READ);
+        PathToUnindexedFieldType pathToUnindexedFieldType = objectFactory.createPathToUnindexedFieldType();
+        pathToUnindexedFieldType.setFieldURI(UnindexedFieldURIType.MESSAGE_IS_READ);
+
+        FieldURIOrConstantType fieldURIOrConstant = objectFactory.createFieldURIOrConstantType();
+        fieldURIOrConstant.setConstant(constantValueType);
+//        fieldURIOrConstant.setPath(objectFactory.createFieldURI(pathToUnindexedFieldType));
+        
+        IsEqualToType filterMessages = new IsEqualToType();
+        filterMessages.setFieldURIOrConstant(fieldURIOrConstant);
+        filterMessages.setPath(objectFactory.createFieldURI(pathToUnindexedFieldType));
+
+        JAXBElement<IsEqualToType> messageReadEqualsFalse = objectFactory.createIsEqualTo(filterMessages);
+
+        RestrictionType restriction = new RestrictionType();
+        restriction.setSearchExpression(messageReadEqualsFalse);
+
+        NonEmptyArrayOfBaseFolderIdsType parentFolderIds = objectFactory.createNonEmptyArrayOfBaseFolderIdsType();
+        parentFolderIds.getFolderIdOrDistinguishedFolderId().add(findInboxFolder(userId).getFolderId());
+
+        IndexedPageViewType indexedPageViewType = objectFactory.createIndexedPageViewType();
+        indexedPageViewType.setBasePoint(IndexBasePointType.BEGINNING);
+        indexedPageViewType.setOffset(0);
+        indexedPageViewType.setMaxEntriesReturned(maxNumber);
+
+        ItemResponseShapeType itemResponseShapeType = objectFactory.createItemResponseShapeType();
+        itemResponseShapeType.setBaseShape(DefaultShapeNamesType.ALL_PROPERTIES);
+
+        FindItemType findItemType = new FindItemType();
+        findItemType.setRestriction(restriction);
+        findItemType.setParentFolderIds(parentFolderIds);
+        findItemType.setTraversal(ItemQueryTraversalType.SHALLOW);
+        findItemType.setIndexedPageItemView(indexedPageViewType);
+        findItemType.setItemShape(itemResponseShapeType);
+
+        Holder<FindItemResponseType> findItemResult = new Holder<FindItemResponseType>();
+
+        ExchangeImpersonationType impersonation = getExchangeImpersonation(userId);
+        exchangeServicePort.findItem(findItemType, impersonation, null, null, null, null, null,
+                findItemResult, null);
+
+        List<JAXBElement<? extends ResponseMessageType>> list = findItemResult.value.getResponseMessages()
+                .getCreateItemResponseMessageOrDeleteItemResponseMessageOrGetItemResponseMessage();
+        FindItemParentType rootFolder = ((FindItemResponseMessageType) list.get(0).getValue()).getRootFolder();
+        List<? extends ItemType> emails = rootFolder.getItems().getItemOrMessageOrCalendarItem();
+
+        /*for (ItemType email : emails) {
+            NonEmptyArrayOfBaseItemIdsType ids = objectFactory.createNonEmptyArrayOfBaseItemIdsType();
+            ids.getItemIdOrOccurrenceItemIdOrRecurringMasterItemId().add(email.getItemId());
+
+            ItemResponseShapeType shapeType = objectFactory.createItemResponseShapeType();
+            shapeType.setBaseShape(DefaultShapeNamesType.ALL_PROPERTIES);
+            shapeType.setBodyType(BodyTypeResponseType.TEXT);
+
+            GetItemType getItemType = new GetItemType();
+            getItemType.setItemIds(ids);
+            getItemType.setItemShape(shapeType);
+
+            Holder<GetItemResponseType> getItemResult = new Holder<GetItemResponseType>();
+
+            exchangeServicePort.getItem(getItemType, impersonation, null, null, null, null, null, getItemResult, null);
+
+            ItemInfoResponseMessageType messageType = (ItemInfoResponseMessageType) getItemResult.value
+                    .getResponseMessages()
+                    .getCreateItemResponseMessageOrDeleteItemResponseMessageOrGetItemResponseMessage()
+                    .get(0).getValue();
+
+            BodyType body = messageType.getItems().getItemOrMessageOrCalendarItem().get(0).getBody();
+            email.setBody(body);
+        }*/
+
+        return (List<MessageType>) emails;
+    }
+
     public List<CalendarItemType> fetchCalendarEvents(String userId, Date startDate, Date endDate) {
         GregorianCalendar startDateCalendar = new GregorianCalendar();
         startDateCalendar.setTime(startDate);
@@ -114,7 +196,8 @@ public class EwsService {
 
         Holder<FindItemResponseType> findItemResult = new Holder<FindItemResponseType>();
 
-        exchangeServicePort.findItem(findItem, exchangeImpersonation, null, null, null, null, null, findItemResult, null);
+        exchangeServicePort.findItem(findItem, exchangeImpersonation, null, null, null, null, null, findItemResult,
+                null);
 
         List<JAXBElement<? extends ResponseMessageType>> responseMessage = findItemResult.value.getResponseMessages()
                 .getCreateItemResponseMessageOrDeleteItemResponseMessageOrGetItemResponseMessage();
@@ -127,20 +210,26 @@ public class EwsService {
 
         JAXBElement<? extends ResponseMessageType> jaxbElement = responseMessage.get(0);
 
-        List<? extends ItemType> items = ((FindItemResponseMessageType) jaxbElement.getValue()).getRootFolder().getItems()
-                .getItemOrMessageOrCalendarItem();
+        List<? extends ItemType> items = ((FindItemResponseMessageType) jaxbElement.getValue()).getRootFolder()
+                .getItems().getItemOrMessageOrCalendarItem();
 
         return (List<CalendarItemType>) items;
     }
 
     public Integer fetchInboxUnreadCount(String userId) {
 
+        FolderType inbox = findInboxFolder(userId);
+
+        return inbox.getUnreadCount();
+    }
+
+    private FolderType findInboxFolder(String userId) {
         Holder<FindFolderResponseType> findFolderResult = new Holder<FindFolderResponseType>();
 
         IndexedPageViewType indexedPageViewType = new IndexedPageViewType();
         indexedPageViewType.setBasePoint(IndexBasePointType.BEGINNING);
         indexedPageViewType.setOffset(0);
-        indexedPageViewType.setMaxEntriesReturned(30);
+        indexedPageViewType.setMaxEntriesReturned(1);
 
         ConstantValueType c1 = new ConstantValueType();
         c1.setValue("Inkorg");
@@ -160,7 +249,8 @@ public class EwsService {
         QName qName = new QName("http://schemas.microsoft.com/exchange/services/2006/types", "IsEqualTo");
 
         RestrictionType restriction = new RestrictionType();
-        restriction.setSearchExpression(new JAXBElement<SearchExpressionType>(qName, SearchExpressionType.class, equalToType));
+        restriction.setSearchExpression(new JAXBElement<SearchExpressionType>(qName, SearchExpressionType.class,
+                equalToType));
 
         DistinguishedFolderIdType distinguishedFolderIdType = new DistinguishedFolderIdType();
         distinguishedFolderIdType.setId(DistinguishedFolderIdNameType.ROOT);
@@ -188,8 +278,8 @@ public class EwsService {
         JAXBElement<? extends ResponseMessageType> jaxbElement = responseMessages
                 .getCreateItemResponseMessageOrDeleteItemResponseMessageOrGetItemResponseMessage().get(0);
 
-        return ((FolderType) (((((FindFolderResponseMessageType) jaxbElement.getValue()).getRootFolder())
-                .getFolders()).getFolderOrCalendarFolderOrContactsFolder()).get(0)).getUnreadCount();
+        return (FolderType) (((((FindFolderResponseMessageType) jaxbElement.getValue()).getRootFolder())
+                .getFolders()).getFolderOrCalendarFolderOrContactsFolder()).get(0);
     }
 
     private ExchangeImpersonationType getExchangeImpersonation(String userId) {
